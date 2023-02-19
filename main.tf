18ed7c05716568ba9e9d6b17452e3274185a1252
armzig <bellotaophyc@outlook.com>
2023-02-19 19:50:38 +0100

    formating the configs

--- main.tf
+++ main.tf
@@ -1,24 +1,4 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "uat" {
-  ami           = "ami-065bb5126e4504910"
-  instance_type = "t2.micro"
-}
-/*
-terraform {
-  backend "s3" {
-    bucket = "works-up-and-running-state"
-    key = "workspaces-uat/terraform.tfstate"
-    region = "eu-west-1"
-    dynamodb_table = "works-up-and-running-state"
-    encrypt = true
-  }
-}
-*/
-
-provider "aws" {
   region = var.region
 }
 
